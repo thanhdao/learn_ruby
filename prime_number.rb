164beb0dfe80fc540fec906f1c83da15425d6bc4
Thanh Dao <thanhdd1@gmail.com>
2017-03-01 08:30:53 +0700

    Prime nnumber

--- prime_number.rb
+++ prime_number.rb
@@ -1,26 +1,20 @@
 #prime_number.rb
 def check_prime_number(num)
   raise RuntimerError.new("number: #{num} must be Integer") if (!num.is_a?(Integer))
-
   return true if (num <=2)
 
-  n = Math.sqrt(num).round
-  
+  n = Math.sqrt(num).round 
   for i in (2..n)
     return false if (num % i == 0)
   end
-
   return true
-
 end
 
-puts " Enter range to check prime number: "
+puts " Find prime number until: "
 
 num = gets.to_i
-for i in (1..num)
-  
-   puts "Prime number: " + i.to_s if check_prime_number(i)
-  
+for i in (1..num)  
+   puts "Prime number: #{i}" if check_prime_number(i)
 end
 
 
