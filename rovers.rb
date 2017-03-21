2ee62831e00238ae33c13178685a38941678dec7
Thanh Dao <thanhdd1@gmail.com>
2017-03-21 16:35:33 +0700

    Update rovers

--- rovers.rb
+++ rovers.rb
@@ -1,8 +1,5 @@
 # rovers.rb
-# $directions = "NESW"
-$directions = {'N'=>0, 'E'=>90, 'S'=>180, 'W'=>270}
 $plateau = {:length => 0, :width => 0 }
-
 class Rovers
   
   def initialize(rover)
@@ -12,44 +9,29 @@ class Rovers
     @direction = rover_paras[2] 
   end
 
-  def move(instruction)
-    angle = $directions[@direction]
+  def move(instruction, move_step)
+    directions = {'N'=>0, 'E'=>90, 'S'=>180, 'W'=>270}
+    angle = directions[@direction]
+
     for i in 0..(instruction.length - 1) do
       raise RuntimerError.new("Wrong instruction: #{instruction[i]}") if !(['L','R','M'].include?(instruction[i]))
-      angle += 270 if instruction[i] == 'L'
-      angle += 90 if instruction[i] == 'R'
-      # angle += (instruction[i] == 'L' ? 270 : (instruction[i] == 'R' ? 90 : 0)) 
+      angle += (instruction[i] == 'L' ? 270 : (instruction[i] == 'R' ? 90 : 0)) 
       angle %= 360
 
       if instruction[i] == 'M'
-        case angle
-          when 90
-            raise RuntimerError.new("Out of Plateau x: #{@x}") if @x + 1 > $plateau[:length]
-            @x += 1
-          when 270
-            raise RuntimerError.new("Out of Plateau x: #{@x}") if @x -1 < 0
-            @x -= 1
-          when 0
-            raise RuntimerError.new("Out of Plateau y: #{@y}") if @y + 1 > $plateau[:width] 
-            @y += 1
-          when 180
-            raise RuntimerError.new("Out of Plateau y: #{@y}") if @y - 1 < 0
-            @y -= 1
-          else
-        end
-        # @x += (angle == 90 ? 1 : (angle == 270 ? -1 : 0))
-        # @y += (angle == 0 ? 1 : (@sangle == 180 ? -1 : 0))
+        x_move = @x + (move_step * Math.sin(angle/180 * Math::PI)).to_i;
+        y_move = @y + (move_step * Math.cos(angle/180 * Math::PI)).to_i; 
+        raise RuntimerError.new("Out of Plateau x, y: (#{x}, #{y})") if x_move < 0 || x_move > $plateau[:length] || y_move < 0 || y_move > $plateau[:width]
+        @x, @y = x_move, y_move
       end
-
     end
 
-    @direction = $directions.key(angle)
+    @direction = directions.key(angle)
   end
 
   def status
     return "#{@x} #{@y} #{@direction}"
   end
-
 end
 
 instructions = IO.readlines("instructions.txt")
@@ -63,10 +45,9 @@ rovers = Array.new
 while i < (instructions.length - 1)
   j = i/2
   rovers[j] = Rovers.new(instructions[i].chomp)
-  rovers[j].move(instructions[i + 1].chomp)
+  rovers[j].move(instructions[i + 1].chomp, 1)
   p rovers[j].status
   i += 2
 end
-p rovers
 
 
